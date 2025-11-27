cde322c542cc59134a2cbe8b4df8a144768310c7
hudaharnoor-commits <hudaharnoor@gmail.com>
2025-11-27 13:50:55 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Controller, useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form"; 
 import { toast } from "sonner";
 import * as z from "zod";
 
